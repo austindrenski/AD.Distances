813997a96b9b5b9f4a4fdebd329a8bb86b022d27
Austin Drenski <austin.drenski@gmail.com>
2017-08-25 16:58:56 -0400

    Changing to xunit tests for vs

--- AD.Distances.Tests/GreatCircleDistance.cs
+++ AD.Distances.Tests/GreatCircleDistance.cs
@@ -1,24 +1,33 @@
 using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 using Xunit;
 
 namespace AD.Distances.Tests
 {
+    [PublicAPI]
     public class GreatCircleDistance
     {
-        private const double Tolerance = 1e-15;
-        
-        private static readonly Coordinates NewYorkCity = new Coordinates(40.7143528, -74.0059731);
+        public static readonly Coordinates NewYorkCity = new Coordinates(40.7143528, -74.0059731);
 
-        private static readonly Coordinates Chicago = new Coordinates(41.8781136, -87.6297982);
+        public static readonly Coordinates Chicago = new Coordinates(41.8781136, -87.6297982);
+
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] { NewYorkCity, NewYorkCity, 0.0 };
+                yield return new object[] { NewYorkCity, Chicago, 1144.0 };
+            }
+        }
 
         [Theory]
-        [MemberData(nameof(NewYorkCity), nameof(NewYorkCity), 0.0)]
-        [MemberData(nameof(NewYorkCity), nameof(Chicago), 1144.0)]
+        [MemberData(nameof(TestData))]
         public void GreatCircleDistanceTest(Coordinates a, Coordinates b, double expected)
         {
             double distance = Coordinates.GreatCircleDistance(a, b);
 
-            Assert.True(Math.Abs(expected - distance) < Tolerance);
+            Assert.True(Math.Abs(expected - distance) < 0.5);
         }
     }
 }
